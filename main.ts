4927057b32cec6402ce329ab2a429a0e0a47b477
Dishit79 <Dishit79@gmail.com>
2022-07-24 18:29:17 -0400

    pass off

--- main.ts
+++ main.ts
@@ -1,17 +1,24 @@
 import { opine, serveStatic, urlencoded, Router } from "https://deno.land/x/opine/mod.ts";
 import { dirname, join } from "https://deno.land/x/opine/deps.ts";
+import { renderFile } from "https://deno.land/x/eta/mod.ts";
 import { api } from "./routes/api.ts"
 
 
 const app = opine()
 const port = 5000
+const __dirname = dirname(import.meta.url)
+
+app.engine(".html", renderFile);
+app.use('/assets', serveStatic(join(__dirname, "assets")));
+app.set("view engine", "html");
+
 app.set("view cache", false)
 app.set('trust proxy', true)
-const __dirname = dirname(import.meta.url)
+app.use(urlencoded());
 app.use("/api", api)
 
 app.get("/", (req,res)=> {
-  res.send("send hi")
+  res.render("dashboard")
 })
 
 app.listen(port);
